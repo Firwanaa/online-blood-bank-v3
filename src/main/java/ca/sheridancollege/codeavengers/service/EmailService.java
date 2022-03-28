5ace661465666ed099fc11f15c6e4dd9679a0a21
firwanaa <firwanaa@sheridancollege.ca>
2022-03-27 23:21:28 -0400

    Added Backend: Declare Emergency

--- src/main/java/ca/sheridancollege/codeavengers/service/EmailService.java
+++ src/main/java/ca/sheridancollege/codeavengers/service/EmailService.java
@@ -1,13 +1,18 @@
 package ca.sheridancollege.codeavengers.service;
 
-
-	
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.CC_EMAIL;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.DEFAULT_PORT;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.EMAIL_SUBJECT;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.FROM_EMAIL;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.GMAIL_SMTP_SERVER;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.PASSWORD;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.SIMPLE_MAIL_TRANSFER_PROTOCOL;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.SMTP_AUTH;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.SMTP_HOST;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.SMTP_PORT;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.SMTP_STARTTLS_ENABLE;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.SMTP_STARTTLS_REQUIRED;
+import static ca.sheridancollege.codeavengers.constants.EmailConstants.USERNAME;
 import static javax.mail.Message.RecipientType.CC;
 import static javax.mail.Message.RecipientType.TO;
 
@@ -22,20 +27,19 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import com.sun.mail.smtp.SMTPTransport;
-import static ca.sheridancollege.codeavengers.constants.EmailConstants.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ca.sheridancollege.codeavengers.domain.User;
 import ca.sheridancollege.codeavengers.repositories.UserRepository;
-
-import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
 	@Autowired
 	private UserRepository userRepository;
+
 	public void sendNewPasswordEmail(String firstName, String password, String email) throws MessagingException {
 		Message message = createEmail(firstName, password, email);
 		SMTPTransport smtpTransport = (SMTPTransport) getEmailSession()
@@ -83,31 +87,38 @@ public class EmailService {
 		return message;
 	}
 
-	private Message createEmergencyRequest(String username, String email, String institutionName) throws MessagingException {
+	private Message createEmergencyRequest(String username, String email, String institutionName)
+			throws MessagingException {
 		Message message = new MimeMessage(getEmailSession());
 		message.setFrom(new InternetAddress(FROM_EMAIL));
 		message.setRecipients(TO, InternetAddress.parse(email, false));
 		message.setRecipients(CC, InternetAddress.parse(CC_EMAIL, false));
 		message.setSubject(EMAIL_SUBJECT);
-		message.setText("Hello " + username + ", \n \n You received An Urgent donation Request From: " + institutionName
+		message.setText("Hello " + username + ", \n \n You received An Urgent donation Request From: "
+				+ institutionName
 				+ "\n \n Online-BloodBank.ca\n \n " +
-				"If you are available and fit to make blood donation, please contact use at inititute_1@email.ca, or Call us at 1800080000\n\n"+
+				"If you are available and fit to make blood donation, please contact use at inititute_1@email.ca, or Call us at 1800080000\n\n"
+				+
 				"Thank you!");
 		message.setSentDate(new Date());
 		message.saveChanges();
 		return message;
 	}
 
-		public void sendEmergencyRequest()
+	public void sendEmergencyRequest()
 			throws MessagingException {
-		List<User> userList = userRepositoy.findAll();
-		Message message = createRequest(username, email, FROM_EMAIL);
-		SMTPTransport smtpTransport = (SMTPTransport) getEmailSession()
-				.getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
-		smtpTransport.connect(GMAIL_SMTP_SERVER, USERNAME, PASSWORD);
-		smtpTransport.sendMessage(message, message.getAllRecipients());
-		smtpTransport.close();
+		List<User> userList = userRepository.findAll();
+		for (User user : userList) {
+			Message message = createRequest(user.getUsername(), user.getEmail(), FROM_EMAIL);
+			SMTPTransport smtpTransport = (SMTPTransport) getEmailSession()
+					.getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
+			smtpTransport.connect(GMAIL_SMTP_SERVER, USERNAME, PASSWORD);
+			smtpTransport.sendMessage(message, message.getAllRecipients());
+			smtpTransport.close();
+		}
+
 	}
+
 	private Session getEmailSession() {
 		Properties properties = System.getProperties();
 		properties.put(SMTP_HOST, GMAIL_SMTP_SERVER);
