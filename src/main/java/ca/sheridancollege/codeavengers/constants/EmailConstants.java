1bdcee0e706602149c3a5a69a9f4270a1e2e3ede
firwanaa <firwanaa@sheridancollege.ca>
2022-09-08 12:31:33 -0400

    Change email serivce to Outlook

--- src/main/java/ca/sheridancollege/codeavengers/constants/EmailConstants.java
+++ src/main/java/ca/sheridancollege/codeavengers/constants/EmailConstants.java
@@ -1,17 +1,31 @@
 package ca.sheridancollege.codeavengers.constants;
 
 public class EmailConstants {
-	public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-	public static final String USERNAME = "online.blood.bank.ca@gmail.com";
+	public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtp";
+	// obbca@outlook.com //same password
+	public static final String USERNAME = "online.blood.ca@outlook.com";
 	public static final String PASSWORD = "N8NLh6JeqA8nz7R";
-	public static final String FROM_EMAIL = "support@onlinebloodbank.ca";
+	public static final String FROM_EMAIL = "online.blood.ca@outlook.com";
 	public static final String CC_EMAIL = "";
 	public static final String EMAIL_SUBJECT = "Online BloodBank, CA - New Password";
-	public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+	public static final String GMAIL_SMTP_SERVER = "smtp-mail.outlook.com";
 	public static final String SMTP_HOST = "mail.smtp.host";
 	public static final String SMTP_AUTH = "mail.smtp.auth";
 	public static final String SMTP_PORT = "mail.smtp.port";
-	public static final int DEFAULT_PORT = 465;
+	public static final String DEFAULT_PORT = "587";
 	public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
 	public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
+	public static final String X_PRIORITY_HEADER = "X-Priority";
+	public static final String X_PRIORITY_HEADER_VALUE = "2";
+	// spring.mail.properties.mail.smtp.connecttimeout=5000
+	// spring.mail.properties.mail.smtp.timeout=3000
+	// spring.mail.properties.mail.smtp.writetimeout=5000
+	// spring.mail.host=smtp.office365.com
+	// spring.mail.password=password
+	// spring.mail.port=587
+	// spring.mail.username=senderemail
+	// spring.mail.properties.mail.smtp.starttls.enable=true
+	// spring.mail.properties.mail.smpt.auth=true
+	// support.email=senderemail
+
 }
--- src/main/java/ca/sheridancollege/codeavengers/service/EmailService.java
+++ src/main/java/ca/sheridancollege/codeavengers/service/EmailService.java
@@ -26,11 +26,12 @@ import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-import com.sun.mail.smtp.SMTPTransport;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sun.mail.smtp.SMTPTransport;
+
+import ca.sheridancollege.codeavengers.constants.EmailConstants;
 import ca.sheridancollege.codeavengers.domain.Donor;
 import ca.sheridancollege.codeavengers.repositories.UserRepository;
 
@@ -54,6 +55,7 @@ public class EmailService {
 		message.setFrom(new InternetAddress(FROM_EMAIL));
 		message.setRecipients(TO, InternetAddress.parse(email, false));
 		message.setRecipients(CC, InternetAddress.parse(CC_EMAIL, false));
+		message.setHeader(EmailConstants.X_PRIORITY_HEADER, EmailConstants.X_PRIORITY_HEADER_VALUE);
 		message.setSubject(EMAIL_SUBJECT);
 		message.setText("Hello " + name + ", \n \n Your new account password is: " + password
 				+ "\n \n Online-BloodBank.ca");
@@ -70,6 +72,7 @@ public class EmailService {
 				.getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
 		smtpTransport.connect(GMAIL_SMTP_SERVER, USERNAME, PASSWORD);
 		smtpTransport.sendMessage(message, message.getAllRecipients());
+		System.out.println("*********************************************************  Request sent");
 		smtpTransport.close();
 	}
 
@@ -79,6 +82,7 @@ public class EmailService {
 		message.setFrom(new InternetAddress(FROM_EMAIL));
 		message.setRecipients(TO, InternetAddress.parse(email, false));
 		message.setRecipients(CC, InternetAddress.parse(CC_EMAIL, false));
+		message.setHeader(EmailConstants.X_PRIORITY_HEADER, EmailConstants.X_PRIORITY_HEADER_VALUE);
 		message.setSubject(EMAIL_SUBJECT);
 		message.setText("Hello " + username + ", \n \n You received A donation Request From: " + institutionName
 				+ "\n \n Online-BloodBank.ca");
@@ -93,6 +97,7 @@ public class EmailService {
 		message.setFrom(new InternetAddress(FROM_EMAIL));
 		message.setRecipients(TO, InternetAddress.parse(email, false));
 		message.setRecipients(CC, InternetAddress.parse(CC_EMAIL, false));
+		message.setHeader(EmailConstants.X_PRIORITY_HEADER, EmailConstants.X_PRIORITY_HEADER_VALUE);
 		message.setSubject(EMAIL_SUBJECT);
 		message.setText("Hello " + username + ", \n \n You received An Urgent donation Request From: "
 				+ institutionName
@@ -109,7 +114,7 @@ public class EmailService {
 			throws MessagingException {
 		List<Donor> userList = userRepository.findAll();
 		for (Donor user : userList) {
-			Message message = createRequest(user.getUsername(), user.getEmail(), FROM_EMAIL);
+			Message message = createEmergencyRequest(user.getUsername(), user.getEmail(), FROM_EMAIL);
 			SMTPTransport smtpTransport = (SMTPTransport) getEmailSession()
 					.getTransport(SIMPLE_MAIL_TRANSFER_PROTOCOL);
 			smtpTransport.connect(GMAIL_SMTP_SERVER, USERNAME, PASSWORD);
@@ -124,8 +129,10 @@ public class EmailService {
 		properties.put(SMTP_HOST, GMAIL_SMTP_SERVER);
 		properties.put(SMTP_AUTH, true);
 		properties.put(SMTP_PORT, DEFAULT_PORT);
-		properties.put(SMTP_STARTTLS_ENABLE, true);
+		properties.put("mail.smtp.ssl.trust", "*");
 		properties.put(SMTP_STARTTLS_REQUIRED, true);
+		properties.put(SMTP_STARTTLS_ENABLE, true);
+		properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
 		return Session.getInstance(properties, null);
 	}
 }
