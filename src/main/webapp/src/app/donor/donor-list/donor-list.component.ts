75fd608484b347a02b2d4bfed03971b9886a6f51
kaur8470 <kaur8470@sheridancollege.ca>
2022-10-27 11:22:17 -0400

    css

--- src/main/webapp/src/app/donor/donor-list/donor-list.component.ts
+++ src/main/webapp/src/app/donor/donor-list/donor-list.component.ts
@@ -108,17 +108,20 @@ export class DonorListComponent implements OnInit {
 
     hospital_icon = {
         url: 'src/assets/hospital.png', // url
-        scaledSize: new google.maps.Size(50, 50), // scaled size
+        scaledSize: new google.maps.Size(40, 40), // scaled size
     };
 
     person_icon = {
         url: 'src/assets/person.png', // url
-        scaledSize: new google.maps.Size(50, 50), // scaled size
+        scaledSize: new google.maps.Size(30, 30), // scaled size
     };
     initMap(donors: Donor[]): void {
         var markers = [];
         const myLatLng = { lat: 43.6532, lng: -79.3832 };
         const institute = { lat: 43.65602, lng: -79.7387 };
+        const institute2 = { lat: 43.7473, lng: -79.7435 };
+         const institute3 = { lat: 43.3332, lng: -79.4210 };
+          const institute4 = { lat: 43.6590, lng: -79.3885 };
         const map = new google.maps.Map(
             document.getElementById('map') as HTMLElement,
             {
@@ -143,6 +146,26 @@ export class DonorListComponent implements OnInit {
             title: 'institution#1!',
             icon: this.hospital_icon,
         });
+        
+        new google.maps.Marker({
+            position: institute2,
+            map,
+            title: 'institution#2!',
+            icon: this.hospital_icon,
+        });
+        new google.maps.Marker({
+            position: institute3,
+            map,
+            title: 'institution#3!',
+            icon: this.hospital_icon,
+        });
+        new google.maps.Marker({
+            position: institute4,
+            map,
+            title: 'institution#4!',
+            icon: this.hospital_icon,
+        });
+        
 
             markers.forEach((marker) => {
         marker.addListener("click", () => {
